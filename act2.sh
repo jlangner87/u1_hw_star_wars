45f6a7822f69d86a24f2d10458112b161e4f3590
Josh Langner <josh.langner@gmail.com>
2022-06-22 08:42:42 -0500

    finished starwars

--- act2.sh
+++ act2.sh
@@ -1,13 +1,14 @@
 # 1. Inside the `star_wars/rebellion` directory, (IN ONE COMMAND using &&) create a file called princess_leia.txt with the text "Help me, Obi-Wan…"
-
+check
 # 2. Create a file called obi_wan.txt in star_wars/rebellion
-
+check
 # 3. Create a file in star_wars/rebellion called luke_skywalker.txt
-
+check
 # 4. Create a directory in star_wars/rebellion called millenium_falcon
-
+check
 # 5. Inside the millenium_falcon, create two files: han_solo.txt and chewbacca.txt
-
+check
 # 6. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
-
+Luke check // Obu check // Leia check
 # 7. Move the millenium_falcon into the death_star.
+check
--- act3.sh
+++ act3.sh
@@ -1,13 +1,14 @@
 # 1. Unload the Millenium Falcon in ONE COMMAND!
 # Move the whole crew from the millenium_falcon directory into the death_star directory. HINT: * following a directory will grab all files/folders inside of a directory (directory/*)
-
+I did it
 # 2. darth_vader has defeated obi_wan! Delete poor obi_wan.
-
+I did it
 # 3.  Our heroes have disabled the tractor beam! Move the whole crew back into the millenium_falcon!
 # Remember: darth_vader remains in the death_star and emperor_palpatine is still in the empire.
-
+I did it
 # 4. Move the millenium_falcon back into the rebellion directory.
-
+I did it
 # 5. darth_vader leaves the death_star to pursue luke_skywalker! Move him from the death_star into the empire directory!
-
+I did it
 # 6. Thanks to his practice back home at Beggar’s Canyon, Luke blew up the death_star! Remove it from the galaxy!
+I did it
